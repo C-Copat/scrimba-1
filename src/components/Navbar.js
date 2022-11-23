d0163696acdfaea995b4d7e9eb597d52f070f7fc
Caetano Copat <caetanocopat@gmail.com>
2022-11-23 01:21:55 -0300

    primeiro-commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import "./Navbar.css"
+
+function Navbar() {
+    return(
+    <nav className="div-container" >
+        <h3>ReactFacts</h3>
+        <h4>React Course - Project 1</h4>
+    </nav>
+    )
+}
+
+export default Navbar;
