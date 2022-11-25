7a294cdabc7b5d3eac5711a5dae3bf969b440728
Caetano Copat <caetanocopat@gmail.com>
2022-11-25 19:24:35 -0300

    funcional

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,13 @@
 import "./Navbar.css"
+import logo from '../images/reactjs-icon.png'
 
 function Navbar() {
     return(
     <nav className="div-container" >
-        <h3>ReactFacts</h3>
+        <div className="flexbox-container">
+            <img src={logo} alt="logo"/>
+            <h3>ReactFacts</h3>
+        </div>
         <h4>React Course - Project 1</h4>
     </nav>
     )
